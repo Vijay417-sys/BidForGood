6c7f501caedbd33c49575ae4f0e12fbccf8fa386
Nithyashree758 <nithyashree.gr1909@gmail.com>
2026-07-04 12:25:22 +0530

    userDTO added

--- src/main/java/com/bidforgood/dto/UserDTO.java
+++ src/main/java/com/bidforgood/dto/UserDTO.java
@@ -2,4 +2,73 @@ package com.bidforgood.dto;
 
 public class UserDTO {
 
+	private int userId;
+    private String fullName;
+    private String email;
+    private String password;
+    private String phone;
+    private String address;
+    private String role;
+    private String status;
+    private String createdAt;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
+	@Override
+	public String toString() {
+		return "UserDTO [userId=" + userId + ", fullName=" + fullName + ", email=" + email + ", password=" + password
+				+ ", phone=" + phone + ", address=" + address + ", role=" + role + ", status=" + status + ", createdAt="
+				+ createdAt + "]";
+	}
 }
